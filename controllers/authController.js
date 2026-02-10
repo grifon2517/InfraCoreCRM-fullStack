1925ad7d9a0c9987aa0ddcd2cdd0af2c06c3461e
Gregory Chebatyuk <gchebatyuk@gmail.com>
2026-02-10 15:28:45 +0300

    Установка пакетов Создание базовой структуры Поднятие сервера Проверка запросов через Thunder Client

---
+++ controllers/authController.js
@@ -0,0 +1,66 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+
+    const existingUser = await User.findOne({ login });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      login,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    res.json({ message: "User registered successfully" });
+  } catch (err) {
+    console.error(err); // <-- добавь это
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+
+    // Проверяем, есть ли пользователь
+    const user = await User.findOne({ login });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid login or password" });
+    }
+
+    // Проверяем пароль
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid login or password" });
+    }
+
+    // Создаём JWT
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        login: user.login,
+        role: user.role,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
