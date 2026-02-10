562c439bac96686fc34c61ede7dd92eff21177c0
Gregory Chebatyuk <gchebatyuk@gmail.com>
2026-02-10 16:35:48 +0300

    Создание middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  const token = authHeader.split(" ")[1]; // Bearer TOKEN
+
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // { userId, role, iat, exp }
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+module.exports = authMiddleware;
